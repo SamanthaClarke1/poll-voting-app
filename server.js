5e68b02c9ba1ff943926aba94933c0943ac37f85
Glitch (flint-mountain) <none>
2017-11-06 13:39:25 +0000

    👛📚 Updated with Glitch

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // server.js
 // (reference) https://github.com/cfsghost/passport-github/blob/master/examples/login/app.js
 
+
 // init project
 var express = require('express');
 var passport = require('passport'),
@@ -13,6 +14,7 @@ var methodOverride = require('method-override');
 var GitHubStrategy = require('passport-github2').Strategy;
 var partials = require('express-partials');
 var app = express();
+var passwordHash = require('password-hash');
 
 var mongodb = require('mongodb').MongoClient;
 var url = 'mongodb://guest:'+process.env.MONGO_PASS+'@ds056979.mlab.com:56979/king-fcc';  // Connection URL.
@@ -51,7 +53,7 @@ mongodb.connect(url, function (err, db) {
             return done(null, data);
           }
           else {
-            var toIns = {"id": profile.id, "name": profile.username, "displayName": profile.displayName};
+            var toIns = {"id": profile.id, "name": profile.username, "displayName": profile.displayName, "password": null, "secret": generateSecret(), "github": true};
             if(profile.photos && profile.photos.length > 0) toIns.img = profile.photos[0].value;
             else toIns.img = process.env.NO_IMAGE_URL;
             usersDB.insert(toIns, function(err, data) {
@@ -78,8 +80,17 @@ mongodb.connect(url, function (err, db) {
       done(null, obj);
     });
 
-    passport.use(new LocalStrategy(function(user, pass, done) {
-      
+    passport.use(new LocalStrategy(function(username, password, done) {
+      usersDB.findOne({ name: displayNameToUsername(username) }, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!passwordHash.verify(password, user.password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      });
     }));
     
     passport.use(new GitHubStrategy({
@@ -92,9 +103,13 @@ mongodb.connect(url, function (err, db) {
     app.get("/", function (req, res) {
       res.render('index', { user: req.user });
     });
-
-    app.get('/account', ensureAuthenticated, function(req, res){
-      res.render('account', { user: req.user });
+    
+    app.get('/paccount', ensureAuthenticated, function(req, res) {
+      res.render('account', { user: req.user, public: true });
+    });
+    
+    app.get('/account', ensureAuthenticated, function(req, res) {
+      res.render('account', { user: req.user, public: false });
     });
 
     // GET /auth/github
@@ -134,18 +149,43 @@ mongodb.connect(url, function (err, db) {
     });
     
     app.post('/auth/login', 
-      passport.authenticate('local', { failureRedirect: '/login' }),
+      passport.authenticate('local', { failureRedirect: '/account' }),
       function(req, res, next) {
+        console.log('/auth/login visited!');
         res.redirect('/'); 
       }
     );
     
-    app.post('/auth/signin', 
-      passport.authenticate('local', { failureRedirect: '/login' }),
-      function(req, res, next) {
-        res.redirect('/'); 
+    app.post('/auth/signin', function(req, res, next) {
+      console.log('/auth/signin visited!')
+      if(req.body.password != req.body.confirmpassword && req.body.password != "" && req.username != "") { // TODO: verify this shit on the front end, too.
+        console.log("error on first catch /auth/signin !"); 
+        res.redirect("/"); 
+      } else {
+        var toIns = {
+                      "img": process.env.NO_IMAGE_URL,
+                      "name": displayNameToUsername(req.body.username),
+                      "displayName": req.body.username,
+                      "password": hashOf(req.body.password),
+                      "github": false,
+                      "secret": generateSecret(),
+                      "profileUrl": "https://poll-voting-app.glitch.me/paccount/" + displayNameToUsername(req.body.username)
+                    };
+        usersDB.findOne({"name": toIns.name}, function(err, data) {
+          if(err) throw err;
+          if(!data) {
+            usersDB.insert(toIns, function(err, data){console.log("hi callback!")});
+            console.log(toIns)
+          }
+          else {
+            console.log("shock!");
+            res.redirect("/login")
+          }
+          console.log("smiles!")
+          res.redirect("/login");
+        });
       }
-    );
+    });
 
   } // end of mongo else statement
 });
@@ -173,3 +213,42 @@ function ensureAuthenticated(req, res, next) {
 function displayNameToUsername(displayName) {
   return displayName.split(' ').join('-');
 }
+
+function hashOf(input) {
+  return passwordHash.generate(input);
+}
+
+function generateSecret(verbsl=Math.random() * 1.3, adverbsl=Math.random() * 2.2, directionsl=Math.random() * 1.1, adjectivesl=Math.random() * 2.2, nounsl=Math.random() * 1.5) {
+  var verbs = ["jumped", "encircled", "dug", "cut", "ate", "licked", "demolished", "chowed down", "obliterated", "lit", "bent", "snapped", "popped", "obfuscated", "obscured", "loaded", "abducted", "stole", "exited", "shot", "told", "loved", "giggled", "wired", "detached", "screwed", "drank", "unscrewed", "swallowed", "sounded"];
+  var adverbs = ["quickly", "slowly", "cleanly", "smoothly", "roughly", "clearly", "air-headidly", "cowardly", "nervously", "lightly", "heavily", "thunderously", "uproarisly", "sleepily", "righteously", "sharply", "bluntly", "strongly"];
+  var directions = ["over", "under", "through", "between", "around"];
+  var adjectives = ["gross", "putrid", "starving", "yummy", "white", "loudly", "black", "yellow", "circular", "blue", "mauve", "purple", "big", "red", "green", "communal", "dirty", "clean", "neat", "hard", "rough", "smooth", "glassy", "shiny", "cowardly", "happy", "sad", "mean", "average", "consumerist", "stunned", "well-known", "open", "funny", "real-life", "handsome", "ugly", "cute", "weird"];
+  var nouns = ["dog", "cat", "phone", "lighter", "pliers", "batteries", "rock", "ruck-shack", "cubby", "bottle", "can", "roll of tape", "paper", "notebook", "bubble-wrap", "human", "alien", "demon", "porch", "guest", "wishbone", "secret", "joke", "date", "actor", "date-astrophy", "kitten", "sparks", "booklet", "glass-bottle", "wristband", "knife", "fork", "brush", "deodarant", "dart", "tape", "wire", "cable", "monitor", "keyboard", "harp", "guitar", "bass", "drumkit", "celebrity"];
+  
+  var sentence = "";
+  var capitalization = Math.floor(Math.random() * 2);
+
+  sentence = "the " + getRandIndsOf(nouns, nounsl)  +     " ";
+  sentence +=    getRandIndsOf(verbs, verbsl)           +     " ";
+  sentence +=    getRandIndsOf(adverbs, adverbsl)       +     " ";
+  sentence +=    getRandIndsOf(directions, directionsl) + " the ";
+  sentence +=    getRandIndsOf(adjectives, adjectivesl) +     " ";
+  sentence +=    getRandIndsOf(nouns, nounsl)           +      "";
+  
+  var nsentence = "";
+  for(var i in sentence) {
+	if(capitalization != 0 && i % capitalization == 0) nsentence += sentence[i].toUpperCase();
+	else nsentence += sentence[i].toLowerCase();
+  }
+
+  return nsentence.split(" ").join("_");
+}
+function getRandIndsOf(arr, amount=1) {
+  var retStr = "";
+  for(var i = 0; i < amount; i++) {
+    retStr += arr[Math.floor(Math.random() * arr.length)];
+    if(i < amount - 1) retStr += " and "; // if not the last one
+  }
+  return retStr;
+}
+// https://pastebin.com/LSWEmQwT
